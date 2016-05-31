f907351580574c0cf61a13bf1fee34b09aa3f371
Andrey Bulat <andrii.bulat@gmail.com>
2016-05-31 16:01:54 +0300

    Added baisc tying between models and views

--- dialog.cpp
+++ dialog.cpp
@@ -6,9 +6,40 @@ Dialog::Dialog(QWidget *parent) :
     ui(new Ui::Dialog)
 {
     ui->setupUi(this);
+
+    model = new QStringListModel(this);
+    QStringList list;
+    list << "First Item" << "Second Item" << "Third Item" << "I'm Original";
+
+    model->setStringList(list);
+
+    ui->listView->setModel(model);
+    ui->cbModelExample->setModel(model);
+
+    ui->listView->setEditTriggers(QAbstractItemView::AnyKeyPressed | QAbstractItemView::DoubleClicked);
 }
 
 Dialog::~Dialog()
 {
     delete ui;
+}
+
+void Dialog::on_pushButton_clicked()
+{
+    // Create
+}
+
+void Dialog::on_pushButton_3_clicked()
+{
+    // Update
+}
+
+void Dialog::on_pushButton_2_clicked()
+{
+    // Delete
+}
+
+void Dialog::on_pushButton_4_clicked()
+{
+    // Sort
 }
--- dialog.h
+++ dialog.h
@@ -1,6 +1,8 @@
 #ifndef DIALOG_H
 #define DIALOG_H
 
+#include <QtCore>
+#include <QtGui>
 #include <QDialog>
 
 namespace Ui {
@@ -14,6 +16,16 @@ class Dialog : public QDialog
 public:
     explicit Dialog(QWidget *parent = 0);
     ~Dialog();
+    QStringListModel *model;
+
+private slots:
+    void on_pushButton_clicked();
+
+    void on_pushButton_3_clicked();
+
+    void on_pushButton_2_clicked();
+
+    void on_pushButton_4_clicked();
 
 private:
     Ui::Dialog *ui;
